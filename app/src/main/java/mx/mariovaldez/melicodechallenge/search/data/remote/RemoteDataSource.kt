3ef705eb14b3668a1060e0869742c7d431b20434
Mario Antonio Flores Valdez <mario.flores@coppel.com>
2023-07-26 11:02:25 -0700

    update repository

--- app/src/main/java/mx/mariovaldez/melicodechallenge/search/data/remote/RemoteDataSource.kt
+++ app/src/main/java/mx/mariovaldez/melicodechallenge/search/data/remote/RemoteDataSource.kt
@@ -1,21 +1,15 @@
 package mx.mariovaldez.melicodechallenge.search.data.remote
 
 import mx.mariovaldez.melicodechallenge.data.remote.services.ApiServices
-import mx.mariovaldez.melicodechallenge.search.presentation.mappers.CategoriesUIMapper
-import mx.mariovaldez.melicodechallenge.search.presentation.mappers.ProductUIMapper
-import mx.mariovaldez.melicodechallenge.search.presentation.models.CategoriesUI
-import mx.mariovaldez.melicodechallenge.search.presentation.models.ProductUI
+import mx.mariovaldez.melicodechallenge.search.data.models.response.Categories
+import mx.mariovaldez.melicodechallenge.search.data.models.response.Product
 import javax.inject.Inject
 
 internal class RemoteDataSource @Inject constructor(
     private val apiServices: ApiServices,
-    private val productUIMapper: ProductUIMapper,
-    private val categoriesUIMapper: CategoriesUIMapper
 ) {
 
-    suspend fun search(query: String): List<ProductUI> =
-        productUIMapper.map(apiServices.search(query).results)
+    suspend fun search(query: String): Product = apiServices.search(query)
 
-    suspend fun categories(): List<CategoriesUI> =
-        categoriesUIMapper.map(apiServices.categories())
+    suspend fun categories(): List<Categories> =apiServices.categories()
 }
