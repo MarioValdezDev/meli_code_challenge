db2df487de9ceb4346e56cef13a9d6c3b89c4b3a
MarioValdezDev <60115634+MarioValdezDev@users.noreply.github.com>
2023-07-26 11:13:35 -0700

    Develop (#9)
    * add timber for logs
    
    * add testing
    
    * update gradle
    
    * update manifest
    
    * update network module
    
    * add categories service
    
    * update kotlin extensions
    
    * add models
    
    * add datasource
    
    * add repository
    
    * add use cases
    
    * create adapters
    
    * create mappers
    
    * delete searchactivty
    
    * add animations
    
    * add drawables
    
    * delete files
    
    * update colors
    
    * update strings
    
    * update styles
    
    * add product detail fragment
    
    * add product list fragment
    
    * add search home fragment
    
    * add search product fragment
    
    * add activity search
    
    * update splash activity
    
    * update bindings
    
    * add item views
    
    * update readme
    
    * update build gradle
    
    * update repository
    
    * remove unused method
    
    * add architecture image
    
    * update readme
    
    * Fix/update architecture to repository (#8)
    
    * update build gradle
    
    * update repository
    
    * remove unused method
    
    * add architecture image
    
    * update readme
    
    ---------
    
    ---------
    
    Co-authored-by: Mario Antonio Flores Valdez <mario.flores@coppel.com>

--- app/src/main/java/mx/mariovaldez/melicodechallenge/search/data/repository/SearchRepository.kt
+++ app/src/main/java/mx/mariovaldez/melicodechallenge/search/data/repository/SearchRepository.kt
@@ -2,16 +2,20 @@ package mx.mariovaldez.melicodechallenge.search.data.repository
 
 import dagger.Reusable
 import mx.mariovaldez.melicodechallenge.search.data.remote.RemoteDataSource
+import mx.mariovaldez.melicodechallenge.search.presentation.mappers.CategoriesUIMapper
+import mx.mariovaldez.melicodechallenge.search.presentation.mappers.ProductUIMapper
 import mx.mariovaldez.melicodechallenge.search.presentation.models.CategoriesUI
 import mx.mariovaldez.melicodechallenge.search.presentation.models.ProductUI
 import javax.inject.Inject
 
 @Reusable
 internal class SearchRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val productUIMapper: ProductUIMapper,
+    private val categoriesUIMapper: CategoriesUIMapper,
 ) {
 
-    suspend fun search(query: String): List<ProductUI> = remoteDataSource.search(query)
+    suspend fun search(query: String): List<ProductUI> = productUIMapper.map(remoteDataSource.search(query).results)
 
-    suspend fun categories(): List<CategoriesUI> = remoteDataSource.categories()
+    suspend fun categories(): List<CategoriesUI> = categoriesUIMapper.map(remoteDataSource.categories())
 }
