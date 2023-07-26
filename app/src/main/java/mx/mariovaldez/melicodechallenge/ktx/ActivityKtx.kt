1bd0d59200d007c1651c721e3108b4c766b97a3c
Mario Antonio Flores Valdez <mario.flores@coppel.com>
2023-07-26 10:25:39 -0700

    update kotlin extensions

--- app/src/main/java/mx/mariovaldez/melicodechallenge/ktx/ActivityKtx.kt
+++ app/src/main/java/mx/mariovaldez/melicodechallenge/ktx/ActivityKtx.kt
@@ -1,19 +1,9 @@
 package mx.mariovaldez.melicodechallenge.ktx
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.viewbinding.ViewBinding
-import mx.mariovaldez.melicodechallenge.app.domain.models.AppPermission
 
 inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
     crossinline bindingInflater: (LayoutInflater) -> T
@@ -23,44 +13,4 @@ inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
 
 fun AppCompatActivity.hideKeyboard() {
     hideKeyboard(currentFocus ?: View(this))
-}
-
-fun AppCompatActivity.copyToClipboard(text: String) {
-    val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-    clipboard.setPrimaryClip(ClipData.newPlainText("", text))
-}
-
-fun AppCompatActivity.isAppPermissionGranted(permission: AppPermission): Boolean =
-    ContextCompat.checkSelfPermission(this, permission.name) ==
-        PackageManager.PERMISSION_GRANTED
-
-fun AppCompatActivity.shouldShowRequestAppPermissionRationale(permission: AppPermission): Boolean =
-    shouldShowRequestPermissionRationale(permission.name)
-
-fun AppCompatActivity.registerActionsForAppPermissionRequest(
-    onGuaranteed: () -> Unit,
-    onDenied: () -> Unit
-): ActivityResultLauncher<String> = registerForActivityResult(RequestPermission()) { isGranted ->
-    if (isGranted) onGuaranteed() else onDenied()
-}
-
-fun AppCompatActivity.launchAppPermissionRequest(
-    requestPermissionLauncher: ActivityResultLauncher<String>,
-    permission: AppPermission
-) = requestPermissionLauncher.launch(permission.name)
-
-fun AppCompatActivity.requestAppPermissions(
-    vararg appPermission: AppPermission,
-    requestCode: Int
-) = requestPermissions(appPermission.map { it.name }.toTypedArray(), requestCode)
-
-fun AppCompatActivity.clearAnimationTransition(): Unit = overridePendingTransition(0, 0)
-
-fun AppCompatActivity.shareImageFromUri(uri: Uri) {
-    val intent = Intent(Intent.ACTION_SEND).apply {
-        putExtra(Intent.EXTRA_STREAM, uri)
-        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        type = "image/png"
-    }
-    startActivity(intent)
 }
--- app/src/main/java/mx/mariovaldez/melicodechallenge/ktx/FragmentKtx.kt
+++ app/src/main/java/mx/mariovaldez/melicodechallenge/ktx/FragmentKtx.kt
@@ -1,15 +1,9 @@
 package mx.mariovaldez.melicodechallenge.ktx
 
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.view.View
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import mx.mariovaldez.melicodechallenge.app.domain.models.AppPermission
+import com.google.android.material.textfield.TextInputEditText
 import mx.mariovaldez.melicodechallenge.ui.delegates.FragmentViewBindingDelegate
 
 fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
@@ -19,24 +13,6 @@ fun Fragment.hideKeyboard() {
     view?.let { activity?.hideKeyboard(it) }
 }
 
-fun Fragment.isAppPermissionGranted(permission: AppPermission): Boolean = context?.let {
-    ContextCompat.checkSelfPermission(it, permission.name) == PackageManager.PERMISSION_GRANTED
-} ?: false
-
-fun Fragment.shouldShowRequestAppPermissionRationale(permission: AppPermission): Boolean =
-    shouldShowRequestPermissionRationale(permission.name)
-
-fun Fragment.registerActionsForAppPermissionRequest(
-    onGuaranteed: () -> Unit,
-    onDenied: () -> Unit
-): ActivityResultLauncher<String> = registerForActivityResult(RequestPermission()) { isGranted ->
-    if (isGranted) onGuaranteed() else onDenied()
+fun Fragment.showKeyboard(searchTextInputEditText: TextInputEditText) {
+    view?.let { activity?.showKeyboard(searchTextInputEditText) }
 }
-
-fun Fragment.launchAppPermissionRequest(
-    requestPermissionLauncher: ActivityResultLauncher<String>,
-    permission: AppPermission
-) = requestPermissionLauncher.launch(permission.name)
-
-fun Fragment.shareImageFromUri(uri: Uri) =
-    (requireActivity() as? AppCompatActivity)?.shareImageFromUri(uri)
